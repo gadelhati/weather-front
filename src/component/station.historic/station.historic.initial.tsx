5baf5288b8d0ee2138f96253b0c48f19e1522eec
GadelhaTI <gadelha.ti@gmail.com>
2023-05-29 23:51:16 -0300

    add station historic

---
+++ src/component/station.historic/station.historic.initial.tsx
@@ -0,0 +1,27 @@
+import { initialCountry } from "../country/country.initial";
+import { initialEquipment } from "../equipment/equipment.initial";
+import { initialInstitution } from "../institution/institution.initial";
+import { initialStationCategory } from "../station.category/station.category.initial";
+import { initialSurveying } from "../surveying/surveying.initial";
+import { StationHistoric } from "./station.historic.interface";
+
+export const initialStationHistoric: StationHistoric = {
+    id: '',
+    localDepth: 0,
+    activation: new Date(),
+    latitude: 0,
+    longitude: 0,
+    marsdenSquare: 0,
+    marsdenSubSquare_1: 0,
+    wmoSquare: 0,
+    marsdenSubSquare_5: 0,
+    platformCode: '',
+    collectionDepth: '',
+    commissionCode: '',
+    
+    stationCategory: initialStationCategory,
+    equipment: initialEquipment,
+    surveying: initialSurveying,
+    institution: initialInstitution,
+    country: initialCountry,
+}
---
+++ src/component/station.historic/station.historic.interface.tsx
@@ -0,0 +1,26 @@
+import { Country } from "../country/country.interface";
+import { Equipment } from "../equipment/equipment.interface";
+import { Institution } from "../institution/institution.interface";
+import { StationCategory } from "../station.category/station.category.interface";
+import { Surveying } from "../surveying/surveying.interface";
+
+export interface StationHistoric {
+    id: string,
+    localDepth: number,
+    activation: Date,
+    latitude: number,
+    longitude: number,
+    marsdenSquare: number,
+    marsdenSubSquare_1: number,
+    wmoSquare: number,
+    marsdenSubSquare_5: number,
+    platformCode: string,
+    collectionDepth: string,
+    commissionCode: string,
+
+    stationCategory?: StationCategory,
+    equipment?: Equipment,
+    surveying?: Surveying,
+    institution?: Institution,
+    country?: Country,
+}
