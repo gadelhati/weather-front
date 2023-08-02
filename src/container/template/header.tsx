2bd7694806772aaebd924fa6e65788e3d13715ae
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2023-08-02 11:25:32 -0300

    shadow

--- src/container/template/header.tsx
+++ src/container/template/header.tsx
@@ -12,7 +12,7 @@ export const Header = styled('div', {
     borderRadius: '.3rem',
     // borderCollapse: 'collapse',
     // border: 'solid $third 1px',
-    boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23), 0 0 5px #aaa inset',
+    boxShadow: '0 .3rem .6rem rgba(0,0,0,0.16), 0 .3rem .6rem rgba(0,0,0,0.23), 0 0 .25rem #AAA inset',
     justifyContent: 'space-between',
     alignItems: 'center',
     // flexWrap: 'wrap',
