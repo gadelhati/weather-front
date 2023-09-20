40db9e18efd9a0401de2f6dc80b1546c63693797
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2023-09-20 18:49:52 -0300

    change base on default config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/weather-front/`,
+  base: `/weather/`,
   plugins: [react()],
   resolve: {
     alias: {
