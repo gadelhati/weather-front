29127559cefc0cc8effb8a0a2dc37e393b46b656
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-02-05 23:23:05 -0300

    chore: change path name

--- src/assets/api/api.tsx
+++ src/assets/api/api.tsx
@@ -4,8 +4,8 @@ import { getAccessToken } from "../../service/service.token"
 const token = getAccessToken();
 
 export const api = axios.create({
-  // baseURL: "http://localhost:3120/weather",
-  baseURL: "https://10.5.193.20/weather",
+  baseURL: "http://localhost:3120/shipsynop",
+  // baseURL: "https://10.5.193.20/shipsynop",
   // headers: { 'content-type': 'application/json' }
 });
 
